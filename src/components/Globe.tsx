f061b64633bde836ebb78f9b41244d95a9301cbc
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-05-24 00:27:13 +0100

    Rotating earth

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -1,5 +1,5 @@
 /* eslint-enable @typescript-eslint/no-unused-vars */
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import * as d3 from "d3"
 import { useEffect } from "react"
 import { geoPath } from "d3"
@@ -7,13 +7,21 @@ import { geoPath } from "d3"
 
 
 export default function Globe({data}:any): React.JSX.Element{
+    const [rotation, setRotation] = useState(0)
     const svgRef:any = useRef(null)
+    const projection = d3.geoOrthographic()
+
     
     useEffect(()=>{
         const svg = d3.select(svgRef.current)
-        const width = 800, height = 400 
+        //const width = 800, height = 400 
+        const speed = 0.004
 
-        const projection = d3.geoOrthographic()
+        
+        setRotation(speed * performance.now())
+        
+
+        projection.rotate([rotation,0,0])
         const pathGenerator:any = geoPath().projection(projection)
 
         svg.selectAll("*.country")
@@ -21,13 +29,15 @@ export default function Globe({data}:any): React.JSX.Element{
             .join("path")
             .attr("class", "country")
             .attr("d", feature => pathGenerator(feature))
+        
+        
 
-    },[data])
+    },[data, projection, rotation])
 
     return(
         <>
         <div className="h-full w-fit mx-auto">
-        <svg width="800px" height="500px" className="m-10 border-2 border-black" ref={svgRef}></svg>
+        <svg width="900px" height="500px" className="m-10 border-2 border-black" ref={svgRef}></svg>
         </div>
         </>
     )
