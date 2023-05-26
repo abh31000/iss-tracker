caa55954ffd2880f412b3af2125c88049f12568a
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-05-26 20:35:40 +0100

    Test

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -1,64 +1,84 @@
-/* eslint-enable @typescript-eslint/no-unused-vars */
 import React, { useRef, useState } from "react"
 import * as d3 from "d3"
 import { useEffect } from "react"
-import { geoPath } from "d3"
-// import ocean from "../assets/ne_110m_ocean.json"
+import { geoInterpolate, geoPath, path } from "d3"
+import axios from "axios"
 
+const client = axios.create({
+    baseURL: "https://api.wheretheiss.at/v1/satellites/25544"
+})
 
 
 export default function Globe({data}:any): React.JSX.Element{
     const [rotation, setRotation] = useState(0)
     const svgRef:any = useRef(null)
-    const projection = d3.geoOrthographic()
-    //const topology = ocean.json()
-    //const [data, setData]:any = useState(null)
-    //const speed = 0.006
-    const sensitivity = 50
+    const projection:any = d3.geoOrthographic()
+    const sensitivity = 75
+    const issCord:any = [-14.599413, -28.673147]
 
     useEffect(()=>{
         const svg = d3.select(svgRef.current).append("svg")
-        //const width = 800, height = 400 
-        
         
 
         const globe = svg.append("circle")
-            .attr("fill", "#EEE")
-            .attr("stroke", "#000")
+            .attr("fill", "#DDDDDD")
+            .attr("stroke", "black")
             .attr("stroke-width", "1px")
             .attr("cx", 400)
             .attr("cy", 330)
             .attr('r',projection.scale())
-        const map = svg.append("g")
         
-            /*svg.append("path")
-            .datum(graticule)
-            .attr("class", "graticule")
-            .attr("d", pathGenerator)*/
+        
+        function updatePP(){
+            //const rot = projection.rotate()
+            const [x, y]:any = projection(issCord)
+
+
+            //console.log(pathGenerator.bounds())
+            const circleGenerator = d3.geoCircle()
+                .center(issCord)
+                .radius(3)
+            
+            const circle = circleGenerator()
+            const draw_circle = pathGenerator(circle)
+            svg.select("#red-dot").remove()
+            
+            
+            //console.log(draw_circle)
+            //console.log(visible)
+            if (draw_circle !== null){
+            const iss = svg.append("circle")
+                .attr('id', "red-dot")
+                .attr('cx', x)
+                .attr('cy', y)
+                .attr('r', 3)
+                .attr('fill', 'red')
+            }
+        }
+
+        d3.timer(updatePP)
 
         
-        //setRotation(speed * performance.now())
-        //if(rotation === 360){setRotation(0)}
+        const map = svg.append("g")
+
         projection.translate([400,330])
         projection.scale(300)
-        //projection.rotate([rotation,0,0])
         let pathGenerator:any = geoPath(projection)
         svg.selectAll("path").attr("d", pathGenerator)
-        const graticule = d3.geoGraticule10()
 
-        
+        // Drawing the globe        
         map
-            .attr("class", "countries")
+            //.attr("class", "countries")
             .selectAll("path")
-            .style("","")
+            //.style("","")
             .data(data.features)
             .enter().append("path")
-            .attr("class", d => "country_" + d.properties.name.replace(" ","_"))
+            //.attr("class", d => "country_" + d.properties.name.replace(" ","_"))
             .attr("d", pathGenerator)
             .style("fill", "white")
             .style('stroke', 'black')
-            .style('stroke-width', 0.7)
-            //.style("opacity",1)
+            .style('stroke-width', 0.5)
+            .style("opacity",0.6)
             
 
             d3.timer(function(elapsed) {
@@ -66,25 +86,11 @@ export default function Globe({data}:any): React.JSX.Element{
                 const k = sensitivity / projection.scale()
                 projection.rotate([
                   rotate[0] - 1 * k,
-                  rotate[1]
+                  0,
                 ])
                 pathGenerator = d3.geoPath(projection)
                 svg.selectAll("path").attr("d", pathGenerator)
               },200)
-            
-            
-
-        /*svg.selectAll("path") //path.featires
-            .data(data.features)
-            //.enter()
-            .join("path")
-            //.append("path")
-            //.attr("class", "continent")
-            .attr("d", feature => pathGenerator(feature))
-            //.attr("d", pathGenerator)
-            .attr("fill","#34A56F")
-            //.style('outline', 'thin solid black')*/
-
         
 
     },[data, projection, rotation])
@@ -93,7 +99,7 @@ export default function Globe({data}:any): React.JSX.Element{
     return(
         <>
         <div className="h-full w-fit mx-auto">
-        <svg width="800px" height="650px" className="border-2  border-black" ref={svgRef}></svg>
+        <svg width="800px" height="650px" className="" ref={svgRef}></svg>
         </div>
         </>
     )
