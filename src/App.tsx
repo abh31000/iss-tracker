49a8ab33655dfe2cfb840f25109cf968138aaea1
abh31000 <abdelkhalekbouklihacene67@gmail.com>
2023-09-07 22:56:25 +0100

    Added vercel web analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Globe from "./components/Globe";
 import globeData from './assets/custom.geo.json'
-
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App() {
    return (
       <>
          <Globe data={globeData}></Globe>
+         <Analytics></Analytics>
       </>
       
    );
